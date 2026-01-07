82bf5f4131bd9268eed4285a11e591a65678784d
Franz Hollerer <franz.hollerer@festo.com>
2026-01-07 08:41:20 +0100

    Adds script to extract false positives from VEX

---
+++ vex-list-false-positive.py
@@ -0,0 +1,63 @@
+#!/usr/bin/python3
+"""List false positive."""
+
+import sys
+import os.path
+import argparse
+import json
+import textwrap
+
+
+def is_file(parser, arg):
+    if not os.path.exists(arg):
+        parser.error(f'The file {arg} does not exist!')
+    else:
+        return arg
+
+
+def parse_arguments():
+    parser = argparse.ArgumentParser(
+        description='List false positive.'
+        )
+    parser.add_argument('--vex',
+                        type=lambda x: is_file(parser, x),
+                        required=True,
+                        help= 'Vulnerability Exploitability eXchange (VEX) '
+                              'file')
+
+    return parser.parse_args()
+
+
+def wrap_and_indent(s):
+    s = textwrap.fill(s, width=76)
+    s = textwrap.indent(s, ' ' * 4)
+    return s
+
+
+def list_false_positive(vex):
+    for vul in vex['vulnerabilities']:
+        if vul['analysis']['state'] == 'false_positive':
+            print(f"{vul['properties'][0]['value']}:")
+            print(f"    Id: {vul['id']}")
+            print()
+            s = wrap_and_indent('Description: ' + vul['description'])
+            print(s)
+            if 'detail' in vul['analysis']:
+                s = wrap_and_indent('Detail: ' + vul['analysis']['detail'])
+                print()
+                print(s)
+            print()
+
+
+def main():
+    args = parse_arguments()
+
+    with open(args.vex) as f:
+        vex = json.load(f)
+
+    list_false_positive(vex)
+
+
+if __name__ == "__main__":
+    main()
+
